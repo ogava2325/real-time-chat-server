47a63eeb25716f43e49fc430b3d4b6fbed1024ac
Dima Paiuk <dimapajuk2325@gmail.com>
2024-12-02 02:13:09 +0200

    Added new origin

--- Chat/Program.cs
+++ Chat/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:3000", "https://icy-stone-073218c1e.4.azurestaticapps.net")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
